109e5347fd72354f1e155b63d2edf19dfaf7beea
lazzy3457 <loicmerte@gmail.com>
2025-12-19 14:20:57 +0100

    v0.1

---
+++ seven_MMI/seven_mmi.js
@@ -0,0 +1,6 @@
+let area1 = document.getElementById("oeil");
+area1.style.backgroundColor = "green";
+area1.style.backgroundColor = "solid 2px black";
+area1.addEventListener("click", () => {
+    console.log("oeil");
+})
