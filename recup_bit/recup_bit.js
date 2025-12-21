f1718fa55cf720a469c918ed4cb4362da8d9e6e8
lazzy3457 <loicmerte@gmail.com>
2025-12-21 18:33:34 +0100

    v0.6

--- recup_bit/recup_bit.js
+++ recup_bit/recup_bit.js
@@ -1,19 +1,82 @@
 
 let div_conteneur_jeu = document.getElementById("conteneur_jeu"); // zone de jeux
 
+class Game {
+
+    constructor() {
+        this.list_bits = [];
+        this.ordinateur = null;
+        this.isRunning = false; // a l'arret
+    }
+
+    init () {
+        this.ordinateur = new Ordinateur("teste", this);
+        this.list_bits.push(new Bit(this));
+    }
+
+    start () {
+        this.init()
+        this.isRunning = true;
+        requestAnimationFrame((time) => this.mainLoop(time));
+    }
+
+    mainLoop () {
+        this.update();
+        this.draw();
+        requestAnimationFrame((time) => this.mainLoop(time));
+    }
+
+    // mise ajour des données de la classe
+    update () {
+        this.ordinateur.update(mousse_x);
+        let alleatoire = nombreAleatoire(1000);
+        if (alleatoire > 900) {
+        this.list_bits.push(new Bit(this));
+            
+        }
+        for (let i = 0; i < this.list_bits.length; i++) {
+            this.list_bits[i].update(1);
+        }
+    }
+
+    // mets ajour l'affichage
+    draw () {
+        this.ordinateur.draw()
+        for (let i = 0; i < this.list_bits.length; i++) {
+            this.list_bits[i].draw();
+        }
+    }
+
+}
+
+// permet de gerer les collision entre les elements
+class CollisionManager {
+
+}
+
 // Est le panier qui recup les bits qui tombes
 class Ordinateur {
 
-    constructor (name) {
+    constructor (name, game) {
         this.score = 0;
         this.vie = 3;
+        this.game = game;
         this.name = name; // nom du panier (joueur)
         this.position_x = div_conteneur_jeu.clientWidth / 2; // mise au centre du panier
         this.position_y = 500; // pas tres utile normalement
         this.size = 100; // en pixels
         this.affichage =  document.createElement("img");
 
-        this.initialisationAffichage()
+        this.init()
+    }
+
+    init () {
+        this.affichage.style.width = this.size + "px"; // taille de l'image
+        this.affichage.style.position = "absolute"; // mise en position absolute pour facilité le mouvement
+        this.affichage.style.left = this.position_x + "px"; // positionement de l'image sur l'axe x
+        this.affichage.style.top = this.position_y + "px"; // positionement de l'image sur l'axe y
+        this.affichage.src = "poulet.jpg"; // image est ...
+        div_conteneur_jeu.appendChild(this.affichage); // insertion de l'image (panier) a l'affichage
     }
 
     perdreVie (perte) {
@@ -24,54 +87,52 @@ class Ordinateur {
         this.score += points; // mise ajour score
     }
 
-    updateAffichage (mousse_x) {
+    // mise ajour des données de la classe
+    update (mousse_x) {
         this.position_x = mousse_x - (this.size / 2); // le mileu de l'image est au dessus de la souris
+    }
+
+    // mets ajour l'affichage
+    draw () {
         this.affichage.style.left = this.position_x + "px"; // mise ajour affichage
-
     }
 
-    initialisationAffichage () {
-        this.affichage.style.width = this.size + "px"; // taille de l'image
-        this.affichage.style.position = "absolute"; // mise en position absolute pour facilité le mouvement
-        this.affichage.style.left = this.position_x + "px"; // positionement de l'image sur l'axe x
-        this.affichage.style.top = this.position_y + "px"; // positionement de l'image sur l'axe y
-        this.affichage.src = "poulet.jpg"; // image est ...
-        div_conteneur_jeu.appendChild(this.affichage); // insertion de l'image (panier) a l'affichage
-    }
 
 }  
 
-const ordinateur = new Ordinateur("C moi"); // creation du panier
-
 // mise a jout des coordonée de la souris
+let mousse_x = 0;
+let mousse_y = 0;
 document.addEventListener('mousemove', (e) => {
-    ordinateur.updateAffichage(e.clientX); 
+    mousse_x = e.clientX;
+    mousse_y = e.clientY;
+    // ordinateur.updateAffichage(e.clientX); 
 });
 
 class Bit {
 
-    static liste_bits = []; // finalement inutile actuellement
-
-    constructor () {
+    constructor (game) {
         this.position_x = nombreAleatoire(div_conteneur_jeu.clientWidth);  // fait apparaitre un bit dans une zone aleatoire de l'ecran  sur l'axe x
         this.position_y = 0;  // positionnement en haut de l'affichage
-        Bit.liste_bits.push(this); 
         this.size = 30;
+        this.game = game;
         this.bit = Math.round(Math.random());
         this.time = 500; // 0.5s
         this.move = 10;
 
         this.affichage = document.createElement("p");
-        this.initialisationAffichage();
+        this.init();
     }
 
-    updateAffichage (deplacement) {
+    update (deplacement) {
         this.position_y += deplacement;
-        this.affichage.style.top = this.position_y + "px";
-
     }
 
-    initialisationAffichage () {
+    draw () {
+        this.affichage.style.top = this.position_y + "px";
+    }
+
+    init () {
         this.affichage.style.width = this.size + "px";
         this.affichage.style.position = "absolute";
         this.affichage.style.left = this.position_x + "px";
@@ -81,17 +142,9 @@ class Bit {
         this.affichage.style.color = "green";
         div_conteneur_jeu.appendChild(this.affichage);
         console.log("je suis creer", this) // juste pour les verif
-
-        // fait bouger le bit de this.move toutes les this.time seconde
-        setInterval(() => {
-            this.updateAffichage(this.move); 
-            console.log('je me suis deplacer de ', this.move)
-        }, this.time);
     }
 
 }
-
-const first_bit = new Bit();
 
 function toBinnaire (score) {
     let binnaire = "";
@@ -108,27 +161,9 @@ function nombreAleatoire(nb) {
     return Math.round(Math.random() * (nb + 1))
 }
 
-
-// let interval = 500; // 0.3s
-// console.log(Bit.liste_bits);
-// setInterval(() => {
-//     if (nombreAleatoire(1000) <= 200) {
-//         let new_bit = new Bit();
-//     }
-//     Bit.liste_bits.forEach(bit_element => {
-//         bit_element.updateAffichage(10); 
-//         console.log('je me suis deplacer de ', 10)
-//     });
-// }, interval);
-
 console.log(toBinnaire(255));
 console.log();
 
-// TIMEUR JEU
-let interval = 500; // 0.5s
-setInterval (() => {
-    if (nombreAleatoire(1000) <= 400) {
-        let new_bit = new Bit();
-    }
-}, interval)
 
+let game = new Game();
+game.start();
